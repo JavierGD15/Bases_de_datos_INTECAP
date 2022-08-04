162812a3fa95660eb2d45bcd323e45fc7a0d7c72
JavierGD15 <3057448870301@ingenieria.usac.edu.gt>
2022-08-03 20:51:21 -0600

    Crear finalizado

--- src/main/java/Proyecto_ventas/SucursalesDAO.java
+++ src/main/java/Proyecto_ventas/SucursalesDAO.java
@@ -1,4 +1,3 @@
-
 package Proyecto_ventas;
 
 import java.sql.Connection;
@@ -6,18 +5,60 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class SucursalesDAO {
+
     PreparedStatement ps;
     ResultSet rs;
     Connection con;
     Conexion conectar = new Conexion();
-   
-    
-    public Object[][] listar_tabla(){
-         String instruccion = "select * from sucursales";
-        
-    
-    
-    return null;
+    Object datos[][];
+
+    public Object[][] listar_tabla() {
+        String instruccion = "select * from sucursales";
+
+        try {
+            int x = 0;
+            con = conectar.Conectar();
+            ps = con.prepareStatement(instruccion);
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+                x++;
+            }
+            datos = new Object[x][5];
+            x = 0;
+            con = conectar.Conectar();
+            ps = con.prepareStatement(instruccion);
+            rs = ps.executeQuery();
+            
+            while (rs.next()) {
+                datos[x][0] = rs.getInt(1);
+                datos[x][1] = rs.getString(2);
+                datos[x][2] = rs.getString(3);
+                datos[x][3] = rs.getString(4);
+                datos[x][4] = rs.getInt(5);
+                x++;
+            }
+
+        } catch (Exception e) {
+        }
+
+        return datos;
     }
     
+    public void crear(int codigo, String nombre, String direccion, String correo, int telefono){
+    String sql = "insert into sucursales(codigo, nombre, direccion, correo, telefono) values (?,?,?,?,?)";
+        try {
+            con = conectar.Conectar();
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, codigo);
+            ps.setString(2, nombre);
+            ps.setString(3, direccion);
+            ps.setString(4, correo);
+            ps.setInt(5, telefono);
+            ps.executeUpdate();
+            
+        } catch (Exception e) {
+        }
+    }
+
 }
--- src/main/java/Proyecto_ventas/sucursales_vistas.java
+++ src/main/java/Proyecto_ventas/sucursales_vistas.java
@@ -1,10 +1,17 @@
 
 package Proyecto_ventas;
 
+import com.mycompany.bases_ejemplo.Bases_ejemplo;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 public class sucursales_vistas {
     JPanel sucursales = new JPanel();
@@ -18,6 +25,15 @@ public class sucursales_vistas {
         JButton crear = new JButton("Crear");
         crear.setBounds(500, 100, 130, 50);
         sucursales.add(crear);
+        
+        ActionListener funcion_crear = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                crear();
+            }
+        };
+
+        crear.addActionListener(funcion_crear);
         
         JButton carga = new JButton("Carga Masiva");
         carga.setBounds(670, 100, 130, 50);
@@ -42,12 +58,82 @@ public class sucursales_vistas {
     private void tabla(){
         
         String columnas []= {"Código","Nombre","Dirección","Correo","Teléfono"};
-        Object filas [][]={{"2020","Arturo","km 36","Correo","777777"}};
+        SucursalesDAO sd = new SucursalesDAO();
+        Object filas [][]= sd.listar_tabla();
         tabla = new JTable(filas,columnas);
         sp = new JScrollPane(tabla);
         sp.setBounds(10, 20, 430, 600);
         sucursales.add(sp);
+    
+    }
+    
+    private void crear(){
+    
+        JFrame frame_sucursal = new JFrame();
+        frame_sucursal.setTitle("Nueva Sucursal");        
+        frame_sucursal.setLocationRelativeTo(null);
+        frame_sucursal.setBounds(50, 175, 350, 400);
+        frame_sucursal.setVisible(true);
         
+        JPanel p1 = new JPanel();
+        p1.setLayout(null);
+        frame_sucursal.add(p1);
+        
+        JLabel l1 = new JLabel("Código");
+        l1.setBounds(50, 20, 80, 50);
+        p1.add(l1);
+        
+        JTextField t1 = new JTextField();
+        t1.setBounds(150, 32, 130, 25);
+        p1.add(t1);
+        
+        JLabel l2 = new JLabel("Nombre");
+        l2.setBounds(50, 80, 80, 50);        
+        p1.add(l2);
+        
+        JTextField t2 = new JTextField();
+        t2.setBounds(150, 92, 130, 25);
+        p1.add(t2);
+        
+        JLabel l3 = new JLabel("Dirección");
+        l3.setBounds(50, 140, 80, 50);
+        p1.add(l3);
+        
+        JTextField t3 = new JTextField();
+        t3.setBounds(150, 152, 130, 25);
+        p1.add(t3);
+        
+        JLabel l4 = new JLabel("Correo");
+        l4.setBounds(50, 200, 80, 50);
+        p1.add(l4);
+        
+        JTextField t4 = new JTextField();
+        t4.setBounds(150, 212, 130, 25);
+        p1.add(t4);
+        
+        JLabel l5 = new JLabel("Télefono");
+        l5.setBounds(50, 260, 80, 50);
+        p1.add(l5);
+        
+        JTextField t5 = new JTextField();
+        t5.setBounds(150, 272, 130, 25);
+        p1.add(t5);
+        
+        JButton b1 = new JButton("Guardar");
+        b1.setBounds(130, 330, 80, 15);
+        p1.add(b1);
+        
+        ActionListener guardar = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                SucursalesDAO sd = new SucursalesDAO();
+                sd.crear(Integer.parseInt(t1.getText()), t2.getText(), t3.getText(), t4.getText(), Integer.parseInt(t5.getText()));
+                frame_sucursal.setVisible(false);
+            }
+        };
+
+        b1.addActionListener(guardar);
+    
     
     
     }
